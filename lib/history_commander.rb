226f9ea21846ef992146b29c5a3354f77a68d0a6
Jeremy Deininger <jeremy@rightscale.com>
2010-07-30 00:28:39 -0700

    new spec, new working pause

--- lib/history_commander.rb
+++ lib/history_commander.rb
@@ -1,24 +1,62 @@
 require 'rubygems'
+require 'json'
 require 'eventmachine'
-require 'ruby-debug'
+require 'eventmachine-tail'
+require 'mq'
+require 'uuidtools'
+require 'mash'
 
+class HistoryCommander < EventMachine::FileTail
+  attr_accessor :uuid
+  attr_accessor :pause
 
-module HistMonitor
-  def file_modified
-    @last_history ||= []
-    @the_history = IO.readlines(path)
-    puts "history file #{path} was modified:"
-    diff = @the_history - @last_history
-    puts "NOOP (dupe entry)" if diff.size == 0
-    puts diff.join("\n")
-    @last_history = @the_history
+  def safe_path
+    "#{path}_safe"
   end
+
+  def initialize(path, startpos=-1, mode="full")
+    super(path, startpos)
+    FileUtils.cp(path, safe_path)
+    @buffer = BufferedTokenizer.new
+    @global_history_fanout = MQ.new.fanout('global_history')
+    @uuid = UUIDTools::UUID.random_create.to_s
+    @host = `hostname`.chomp
+    @user = `whoami`.chomp
+    @pause = false
+    subscribe if mode == "full"
+  end
+
+  def receive_data(data)
+    @buffer.extract(data).each do |line|
+      payload = { :uuid => @uuid, 
+                  :message => line,
+                  :host => @host,
+                  :user => @user }
+                  
+      @global_history_fanout.publish(payload.to_json)
+    end
+  end
+  
+  def subscribe 
+    @subscription = MQ.new
+    @subscription.queue(@uuid).bind(@subscription.fanout('global_history')).subscribe do |result|
+      x = Mash.new(JSON::parse(result))
+      puts "received: #{x[:uuid]} #{x[:user]}@#{x[:host]}$ #{x[:message]}"
+      if x[:uuid] != @uuid
+        @pause = true
+        File.open(path, "a") {|f| f.puts(x[:message])}
+        skip_ahead
+        @pause = false
+        schedule_next_read
+      end
+    end
+  end
+
 end
 
 class HistWatch
-  def self.start
-    myhome = File.join(File.expand_path('~'), ".bash_history")
-    hist_file_watch = EM.watch_file(myhome, HistMonitor)
+  def self.start(file = File.join(File.expand_path('~'), ".bash_history"))
+    @hist_file_watch = EventMachine::file_tail(file, HistoryCommander)
   end
 
   def self.stop
--- spec/history_commander_spec.rb
+++ spec/history_commander_spec.rb
@@ -1,9 +1,43 @@
+# This Spec requires a running AMQP server.
+# Configure your AMQP server first and enter access information below before running this spec.
+
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require 'tmpdir'
 
 describe "HistoryCommander" do
-  it "is cool" do
+  before(:all) do
+    @watch_files = []
+    2.times do |x|
+      @watch_files << File.join(Dir.tmpdir, "#{x}unique-history_commander_spec_candelete")
+    end
+    @watch_files.each {|w| `rm #{w}; touch #{w}`}
+  end
+  it "runs multiple history commanders and checks for all files to contain all history" do
     EM.run do
-      HistWatch.start
+      AMQP.start(:host => "ec2-184-72-19-157.us-west-1.compute.amazonaws.com") do
+        EM.next_tick do
+          @watch_files.each do |w|
+            HistWatch.start(w)
+          end
+        end
+        EM.add_timer(2) do
+          @first_value = "first_command: #{rand(10000000)}"
+          File.open(@watch_files.first, "a") {|f| f.puts @first_value }
+        end
+        EM.add_timer(3) do
+          @second_value = "second_command: #{rand(10000000)}"
+          File.open(@watch_files.last, "a") {|f| f.puts @second_value }
+        end
+        EM.add_timer(10) do
+          @watch_files.each do |w|
+            IO.read(w).should =~ /^#{@first_value}/
+            IO.read(w).should =~ /^#{@second_value}/
+          end
+        end
+        EM.add_timer(12) do
+          EM.stop
+        end
+      end
     end
   end
 end
